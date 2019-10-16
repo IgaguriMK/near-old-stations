e7e65a206e66b2e299b44748e47e6a73701cd6cf
Igaguri <igagurimk@gmail.com>
2019-10-16 21:33:41 +0900

    feat: command line options

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Read;
 
+use clap::{App, Arg, crate_version};
 use serde::Deserialize;
 use tiny_fail::{ErrorMessageExt, Fail};
 use toml::from_slice;
@@ -26,7 +27,34 @@ impl Config {
         f.read_to_end(&mut bytes)
             .err_msg("failed read config file")?;
 
-        let cfg = from_slice(&bytes).err_msg("failed parse config")?;
+        let mut cfg: Config = from_slice(&bytes).err_msg("failed parse config")?;
+
+        // args
+        let matches = App::new("near-old-stations")
+            .version(crate_version!())
+            .arg(Arg::with_name("max_dist").long("max-dist").takes_value(true).help("Maximum distance from current position"))
+            .arg(Arg::with_name("days").long("days").takes_value(true).help("Minimum days from last update"))
+            .arg(Arg::with_name("max_entries").short("n").long("max-entries").takes_value(true).help("Minimum entries to show"))
+            .arg(Arg::with_name("mode").long("mode").takes_value(true).possible_values(&["oneshot", "update"]).help("Run mode"))
+            .get_matches();
+
+        if let Some(s) = matches.value_of("max_dist") {
+            cfg.max_dist = s.parse::<f64>().err_msg("can't parse 'max_dist' as float")?;
+        }
+        if let Some(s) = matches.value_of("days") {
+            cfg.days = s.parse::<i64>().err_msg("can't parse 'days' as int")?;
+        }
+        if let Some(s) = matches.value_of("max_entries") {
+            cfg.max_entries = s.parse::<usize>().err_msg("can't parse 'max_entries' as int")?;
+        }
+        if let Some(s) = matches.value_of("mode") {
+            match s {
+                "oneshot" => cfg.mode = Mode::Oneshot,
+                "update" => cfg.mode = Mode::Update,
+                s => return Err(Fail::new(format!("invalid mode '{}'", s))),
+            }
+        }
+
         Ok(cfg)
     }
 }
