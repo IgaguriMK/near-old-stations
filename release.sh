95bd5ab9c357eb4ec12974ed7881c9f2ecb266bb
Igaguri <igagurimk@gmail.com>
2019-11-27 18:46:13 +0900

    feat: include stats to release

--- release.sh
+++ release.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 rm -r near-old-stations || true
+rm -r *.gz || true
+rm -r .cache.json || true
 
 set -eu
 
-cargo b --release
 mkdir near-old-stations
+
+cargo r --release -- --mode oneshot
+
 cp target/release/near-old-stations.exe near-old-stations/
+cp target/release/stats.exe near-old-stations/
 cp config.sample.toml near-old-stations/config.toml
+cp coordinates.json.gz near-old-stations/
