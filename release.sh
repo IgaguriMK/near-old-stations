58d39f34c03075f068c75382652c4dfadede4b1d
Igaguri <igagurimk@gmail.com>
2019-12-13 16:29:12 +0900

    doc: README.md

--- release.sh
+++ release.sh
@@ -9,8 +9,13 @@ set -eu
 mkdir near-old-stations
 
 cargo r --release -- --mode oneshot
+cargo package
 
 cp target/release/near-old-stations.exe near-old-stations/
 cp target/release/stats.exe near-old-stations/
+cp LICENSE-APACHE near-old-stations/
+cp LICENSE-MIT near-old-stations/
+cp README.md near-old-stations/
+cp CHANGELOG.md near-old-stations/
 cp config.sample.toml near-old-stations/config.toml
 cp coordinates.json.gz near-old-stations/
